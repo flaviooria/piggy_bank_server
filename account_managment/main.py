c5b6b44a6c4b11738bf6d19d5ef8e3a8770636c2
Flavio Oria <flavio.oriap@gmail.com>
2024-07-13 21:21:57 +0200

    🚚 Add all routes for api version 1

--- account_managment/main.py
+++ account_managment/main.py
@@ -5,6 +5,7 @@ from fastapi.responses import RedirectResponse
 from raw_dbmodel import create_tables
 
 from account_managment.accounts.models import Accounts
+from account_managment.api_v1 import api_v1_router
 from account_managment.settings.settings import settings
 from account_managment.user.models import Users
 
@@ -19,9 +20,11 @@ async def lifespan_init_db(_app: FastAPI):
 app = FastAPI(title=settings.APP_NAME,
               version="0.1.0", lifespan=lifespan_init_db, docs_url=f"{settings.API_VERSION}/docs")
 
+app.include_router(api_v1_router)
+
 
 @app.get("/")
-def gretting():
+def greeting():
     return RedirectResponse(f"{settings.API_VERSION}/docs")
 
 
