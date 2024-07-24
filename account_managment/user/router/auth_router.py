67f38caaf770364dd2219f0c851eb76ab492eef5
Flavio Oria <flavio.oriap@gmail.com>
2024-07-24 16:32:42 +0200

    :sparkles: add new endpoints, refresh_token and logout

--- account_managment/user/router/auth_router.py
+++ account_managment/user/router/auth_router.py
@@ -1,10 +1,14 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import (APIRouter, Depends, HTTPException, Request, Response,
+                     status)
 
-from account_managment.shared import CurrentUserDependency, JwtUtil, Payload
-from account_managment.user.dtos import UserCreateDto, UserSingInDto
+from account_managment.settings.settings import settings
+from account_managment.shared import (AuthAccessToken, AuthRefreshToken,
+                                      JwtUtil, Payload)
+from account_managment.user.dtos import (UserCreateDto, UserSingInDto,
+                                         UserWithoutPasswordDto)
 from account_managment.user.models import Users
 from account_managment.user.respositories import UserRepository
 from account_managment.user.services import GetUserService, UserRegisterService
@@ -17,7 +21,37 @@ user_register_service = UserRegisterService(user_repository)
 get_user_service = GetUserService(user_repository)
 
 
-def get_user(payload: Annotated[Payload, Depends(CurrentUserDependency)]):
+def get_expires(expires: str):
+    param, _, time = expires.partition(" ")
+
+    time = int(time)
+    _timedelta = timedelta(minutes=30)
+
+    if param == 'days':
+        _timedelta = timedelta(days=time)
+    if param == 'seconds':
+        _timedelta = timedelta(seconds=time)
+    if param == 'minutes':
+        _timedelta = timedelta(minutes=time)
+    if param == 'hours':
+        _timedelta = timedelta(hours=time)
+
+    return datetime.now(timezone.utc) + _timedelta
+
+
+EXPIRES_ACCESS_TOKEN = get_expires(settings.EXPIRES_ACCESS_TOKEN)
+EXPIRES_REFRESH_TOKEN = get_expires(settings.EXPIRES_REFRESH_TOKEN)
+
+
+def get_user_with_access_token(payload: Annotated[Payload, Depends(
+    AuthAccessToken)]): return get_user_from_payload(payload)
+
+
+def get_user_with_refresh_token(payload: Annotated[Payload, Depends(
+    AuthRefreshToken)]): return get_user_from_payload(payload)
+
+
+def get_user_from_payload(payload: Payload):
     email = payload.sub
 
     user = get_user_service.execute(email)
@@ -57,7 +91,7 @@ def register(user_create: UserCreateDto) -> Any:
 
 
 @auth_router.post('/signin')
-def login(user_signin: UserSingInDto):
+def login(user_signin: UserSingInDto, request: Request, response: Response):
     try:
         user_exist = get_user_service.execute(user_signin.email)
 
@@ -69,22 +103,59 @@ def login(user_signin: UserSingInDto):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Email or Password invalid")
 
-        # TODO: Añadir utilidad para crear jwt
-        expires = datetime.now(tz=timezone.utc) + timedelta(hours=1)
-        payload = Payload(sub=user_exist.email, exp=expires, id=user_exist.id)
+        expires_access_token = EXPIRES_ACCESS_TOKEN
 
-        # return user_exist.model_dump(exclude={"password": True})
-        token = JwtUtil.encode(payload)
+        expires_refresh_token = EXPIRES_REFRESH_TOKEN
 
-        return {"access_token": token, "token_type": "bearer"}
+        payload_access_token = Payload(
+            sub=user_exist.email, exp=expires_access_token, id=user_exist.id)
+
+        payload_refresh_token = Payload(
+            sub=user_exist.email, exp=expires_refresh_token, id=user_exist.id)
+
+        access_token = JwtUtil.encode(payload_access_token)
+        refresh_token = JwtUtil.encode(payload_refresh_token)
+
+        response.set_cookie("access_token", access_token,
+                            httponly=True, expires=expires_access_token)
+        response.set_cookie("refresh_token", refresh_token,
+                            httponly=True, expires=expires_refresh_token)
+
+        user_without_password = user_exist.model_dump(
+            exclude={"password": True})
+
+        return user_without_password
     except HTTPException:
         raise
 
 
-@auth_router.post('/me')
-def get_user(current_user: Annotated[Users, Depends(get_user)]):
+@auth_router.get('/logout')
+def logout(response: Response):
+    response.delete_cookie("access_token", httponly=True)
+    response.delete_cookie("refresh_token", httponly=True)
+
+    return {"message": "Logout successfully"}
+
+
+@auth_router.get('/me', response_model=UserWithoutPasswordDto)
+def current_user(current_user: Annotated[Users, Depends(get_user_with_access_token)]):
     # TODO: Añadir almacenamiento en redis al iniciar sesión el usuario
-    return current_user.model_dump(exclude={"password": True})
+    return current_user.model_dump()
+
+
+@auth_router.post('/refresh_token')
+def refresh_token(current_user: Annotated[Users, Depends(get_user_with_refresh_token)], response: Response):
+
+    expires_access_token = EXPIRES_ACCESS_TOKEN
+    payload_access_token = Payload(
+        sub=current_user.email, exp=expires_access_token, id=current_user.id)
+
+    access_token = JwtUtil.encode(payload_access_token)
+
+    response.set_cookie("access_token", access_token,
+                        httponly=True, expires=expires_access_token)
+
+    return {"message": "Access token refreshed"}
 
 
 __all__ = ["auth_router"]
