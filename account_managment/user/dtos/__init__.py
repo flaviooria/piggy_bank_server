b2c760bc7708310650c73079a0f0fda4af614917
Flavio Oria <flavio.oriap@gmail.com>
2024-07-24 16:31:34 +0200

    :recycle: remove extends from user base

--- account_managment/user/dtos/__init__.py
+++ account_managment/user/dtos/__init__.py
@@ -1,2 +1,4 @@
 from .user_register_dto import UserCreateDto as UserCreateDto
 from .user_signin_dto import UserSingInDto as UserSingInDto
+from .user_without_password import \
+    UserWithoutPasswordDto as UserWithoutPasswordDto
--- account_managment/user/dtos/user_register_dto.py
+++ account_managment/user/dtos/user_register_dto.py
@@ -1,5 +1,10 @@
-from account_managment.user.models.user_model import UserBase
+from pydantic import BaseModel
+
+from account_managment.shared.types import EmailStr
 
 
-class UserCreateDto(UserBase):
+class UserCreateDto(BaseModel):
+    name: str
+    lastname: str
+    email: EmailStr
     password: str
