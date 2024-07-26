1c3783bd59fd70b002c77010157bbbf0a56d8b27
Flavio Oria <flavio.oriap@gmail.com>
2024-07-26 09:36:22 +0200

    :fire: remove dependency current_user

--- account_managment/shared/dependencies/current_user.py
+++ account_managment/shared/dependencies/current_user.py
@@ -1,10 +1,7 @@
 from fastapi import Depends, HTTPException, Request, status
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 
 from account_managment.shared.utils import JwtUtil
-
-security = HTTPBearer()
 
 
 class CookiesToken:
@@ -20,25 +17,6 @@ class CookiesToken:
                 status_code=status.HTTP_403_FORBIDDEN, detail=f"{self.cookie_key} is missing")
 
         return cookies
-
-
-def get_current_user(authorizathion: HTTPAuthorizationCredentials = Depends(security)):
-    token = authorizathion.credentials
-
-    try:
-        payload = JwtUtil.decode(token)
-
-        return payload
-
-    except InvalidSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid signature")
-    except ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Expired token")
-    except DecodeError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Decode error jwt")
 
 
 access_token_security = CookiesToken()
