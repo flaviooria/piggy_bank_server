90477dc0f79e69fd7e3bc8b58f845f605c7a8e94
Flavio Oria <flavio.oriap@gmail.com>
2024-09-19 13:43:55 +0200

    :recycle: changue return types

--- account_managment/accounts/domain/interfaces/account.py
+++ account_managment/accounts/domain/interfaces/account.py
@@ -1,14 +1,13 @@
 from account_managment.common import ICrud
-from account_managment.common.interfaces.crud_interface import AnyModel
 from account_managment.entities.entities import Account
 
 
 class IAccount(ICrud[Account]):
-    async def get_all(self) -> list[AnyModel]:
+    async def get_all(self) -> list[Account]:
         pass
 
-    async def insert(self, model: AnyModel) -> AnyModel:
+    async def insert(self, model: Account) -> Account:
         pass
 
-    async def get_one(self, email: str) -> AnyModel | None:
+    async def get_one(self, email: str) -> Account | None:
         pass
