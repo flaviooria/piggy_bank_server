8d98a3b71471f2ba3e34c412a6d2bea132f5d545
Flavio Oria <flavio.oriap@gmail.com>
2024-12-13 21:43:07 +0100

    ✨ Add methods to save last deploy and rollback service

--- deploy_render.py
+++ deploy_render.py
@@ -11,17 +11,18 @@ from pydantic import BaseModel, RootModel
 class DockerImage(BaseModel):
     ref: str
     sha: str
+    registryCredential: str | None
 
 
 class Deploy(BaseModel):
     id: str
     commit: Any
-    image: DockerImage
+    image: Any
     status: str
     trigger: str
     createdAt: str
-    updatedAt: str
-    finishedAt: str
+    updatedAt: str | None
+    finishedAt: str | None
 
 
 class DeployItem(BaseModel):
@@ -39,12 +40,16 @@ class DeployRenderService:
         self.render_api_key = render_api_key
         self.docker_image_path = docker_image_path
         self.service_id = service_id
+        self.secrets: dict | None = None
 
     async def __resume_service(self) -> None:
         try:
             url = f"https://api.render.com/v1/services/{self.service_id}/resume"
 
-            headers = {"accept": "application/json", "authorization": f"Bearer {self.render_api_key}"}
+            headers = {
+                "accept": "application/json",
+                "authorization": f"Bearer {self.render_api_key}",
+            }
 
             response = requests.post(url, headers=headers)
 
@@ -56,25 +61,11 @@ class DeployRenderService:
         except Exception:
             raise
 
-    async def __get_last_deploy(self) -> str:
-        try:
-            headers = {"accept": "application/json", "authorization": f"Bearer {self.render_api_key}"}
-
-            response = requests.get(self.url_deploys, headers=headers)
-
-            if response.status_code == 200:
-                deploys = Deploys.model_validate(response.json())
-                return deploys.root[0].deploy.id
-
-            raise Exception("Error to get last deploy id")
-        except Exception:
-            raise
-
     async def __rollback_last_deploy(self) -> None:
         try:
             url = f"https://api.render.com/v1/services/{self.service_id}/rollback"
 
-            deploy_id = await self.__get_last_deploy()
+            deploy_id = await self.__get_last_success_deploy()
 
             payload = {"deployId": deploy_id}
 
@@ -95,7 +86,7 @@ class DeployRenderService:
         except Exception:
             raise
 
-    async def __deploy_service(self) -> bool:
+    async def __deploy_service(self) -> str | None:
         try:
             await self.__resume_service()
 
@@ -109,21 +100,145 @@ class DeployRenderService:
             response = requests.post(self.url_deploys, json=payload, headers=headers)
 
             if response.status_code == 201:
-                print("Deployment successful")
-                return True
+                print("Deployment triggered")
 
-            return False
+                deploy = Deploy.model_validate(response.json())
+
+                return deploy.id
+
+            return None
         except Exception:
             raise
 
     async def generate_deploy(self):
+        service_deployed_id = await self.__deploy_service()
 
-        service_deployed = await self.__deploy_service()
-
-        if not service_deployed:
+        if service_deployed_id is None:
             await self.__rollback_last_deploy()
 
+        while True:
+            status = await self.__check_status_deploy(service_deployed_id)
+
+            if status == "WAITING":
+                await asyncio.sleep(5)
+                continue
+
+            if status == "SUCCESS":
+                break
+
+            if status == "FAILED":
+                print("Service deploy is status failed")
+                print("Wait for a rollback to the previous deployment")
+                await self.__rollback_last_deploy()
+                return
+
+        await self.__update_last_success_deploy(service_deployed_id)
         print("Your service is run now")
+
+    async def __check_status_deploy(self, deploy_id: str):
+        try:
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "authorization": f"Bearer {self.render_api_key}",
+            }
+
+            "https://api.render.com/v1/services/srv-cqh6do5ds78s73b0ldgg/deploys/dep-cte6jbdumphs73dfarm0"
+            response = requests.get(f"{self.url_deploys}/{deploy_id}", headers=headers)
+
+            if response.status_code == 200:
+                deploy = Deploy.model_validate(response.json())
+
+                if deploy.status in ["build_in_progress", "update_in_progress"]:
+                    return "WAITING"
+
+                if deploy.status == "build_failed":
+                    return "FAILED"
+
+                if deploy.status == "live":
+                    return "SUCCESS"
+
+            return "FAILED"
+
+        except Exception:
+            raise
+
+    async def __get_list_secrets(self) -> dict | None:
+        """
+        Get list of secrets from doppler
+        """
+        try:
+            url = (
+                "https://api.doppler.com/v3/configs/config/secrets?project=piggy_bank_server"
+                "&config=ci&include_dynamic_secrets=false&include_managed_secrets=true"
+            )
+
+            headers = {
+                "accept": "application/json",
+                "accepts": "application/json",
+                "authorization": f"Bearer {os.environ.get('DOPPLER_TOKEN')}",
+            }
+
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                self.secrets = response.json()
+                return response.json()
+
+            return None
+        except Exception:
+            raise
+
+    async def __get_last_success_deploy(self) -> str | None:
+        """
+        Get last id of service deploy with status live from doppler
+        """
+        try:
+            list_secrets = await self.__get_list_secrets()
+
+            if list_secrets is None:
+                return None
+
+            secrets: dict = list_secrets.get("secrets")
+
+            last_success_deploy = secrets.get("LAST_SUCCESS_DEPLOY", None)
+
+            if last_success_deploy is None:
+                return None
+
+            return last_success_deploy["computed"]
+        except Exception:
+            raise
+
+    async def __update_last_success_deploy(self, deploy_id: str):
+        try:
+            url = "https://api.doppler.com/v3/configs/config/secrets"
+
+            # if self.secrets is None:
+            #     await self.__get_list_secrets()
+
+            # self.secrets.update()
+
+            payload = {
+                "project": "piggy_bank_server",
+                "config": "ci",
+                "secrets": {"LAST_SUCCESS_DEPLOY": deploy_id},
+            }
+
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {os.environ.get('DOPPLER_TOKEN')}",
+            }
+
+            response = requests.post(url, json=payload, headers=headers)
+
+            if response.status_code != 200:
+                print("The last sucess deploy id is not saved in store")
+                return
+
+            print("Deploy id is saved in store")
+        except Exception:
+            raise
 
 
 if __name__ == "__main__":
@@ -140,7 +255,9 @@ if __name__ == "__main__":
             raise ValueError("DOCKER_IMAGE_PATH is not set")
 
         deploy_render_service = DeployRenderService(
-            render_api_key=RENDER_API_KEY, docker_image_path=DOCKER_IMAGE_PATH, service_id=SERVICE_ID
+            render_api_key=RENDER_API_KEY,
+            docker_image_path=DOCKER_IMAGE_PATH,
+            service_id=SERVICE_ID,
         )
 
         asyncio.run(deploy_render_service.generate_deploy())
